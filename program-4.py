72ffa5645fb0d902c576565d1dcfa14966426075
b.tanmay <get.reply.com@gmail.com>
2026-05-12 16:16:45 +0530

    done program 4

---
+++ program-4.py
@@ -0,0 +1,15 @@
+# Write a Python program to swap two variables
+
+
+a  = 5
+b = 10
+print(f"orignal  value , a = {a} ,  b = {b}")
+#lets crate a Temp verible 
+
+temp = a
+
+a  = b
+
+b = temp
+
+print(f"current  value , a = {a} ,  b = {b}")
